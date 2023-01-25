216e3a8dff0299983e76495262cd878352168e58
Jason Andrews <jaandrews@clarku.edu>
2023-01-25 18:19:39 -0500

    test from lab computer

---
+++ R/my_number_checker.R
@@ -0,0 +1,21 @@
+#' A number-checking function
+#'
+#' @description This function allows you to test whether a number falls into the
+#' Goldilocks range (5-9) or not
+#' @param x A number
+#' @export
+#' @examples
+#' my_number_checker(1)
+#' my_number_checker(7)
+#' my_number_checker(11)
+
+
+my_number_checker <- function(x) {
+  if(x < 5) {
+    print("Too low!")
+  } else if(x >= 5 & x < 10) {
+    print("Just right!!! :)")
+  } else {
+    print("Too high!")ss
+  }
+}
